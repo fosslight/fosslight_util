317db4bed40281aef8bd31828d75c52a3f038d84
Soim Kim <soim.kim@lge.com>
2021-04-29 09:41:59 +0900

    Add function for log item

--- tests/test_log.py
+++ tests/test_log.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2021 LG Electronics Inc.
 # SPDX-License-Identifier: Apache-2.0
-
+import yaml
 from fosslight_util.set_log import init_log
+from fosslight_util.set_log import init_log_item
 from _print_log import print_log
 from _print_log_with_another_logger import print_log_another_logger
 
@@ -11,6 +12,10 @@ def main():
     logger = init_log("test_result/log_file1.txt")
     logger.warning("TESTING LOG - from 1st Module")
     
+    result_log = init_log_item("fosslight_util")
+    _str_final_result_log = yaml.safe_dump(result_log, allow_unicode=True)
+    logger.warning(_str_final_result_log)
+
     print_log()
     print_log_another_logger()
 
