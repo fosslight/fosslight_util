d79e4fbd16ef03a4616101ae673aec9a87f64318
Jiyeong Seok <jiyeong.seok@lge.com>
2022-05-18 10:53:52 +0900

    Delete the duplicated files, sources
    Signed-off-by: Jiyeong Seok <jiyeong.seok@lge.com>

--- src/fosslight_util/oss_item.py
+++ src/fosslight_util/oss_item.py
@@ -14,8 +14,6 @@ class OssItem:
         self._name = "-"
         self._version = ""
         self._licenses = []
-        self.source = ""
-        self._files = []
         self._copyright = ""
         self.comment = ""
         self._exclude = False
@@ -23,7 +21,6 @@ class OssItem:
         self.relative_path = value
         self._source_name_or_path = []
         self.download_location = ""
-        self._copyright_text = ""
 
     def __del__(self):
         pass
@@ -39,18 +36,6 @@ class OssItem:
                 value = "\n".join(value)
             value = value.strip()
         self._copyright = value
-
-    @property
-    def copyright_text(self):
-        return self._copyright_text
-
-    @copyright_text.setter
-    def copyright_text(self, value):
-        if value != "":
-            if isinstance(value, list):
-                value = "\n".join(value)
-            value = value.strip()
-        self._copyright_text = value
 
     @property
     def exclude(self):
@@ -96,27 +81,15 @@ class OssItem:
         self._licenses = list(set(self._licenses))
 
     @property
-    def files(self):
-        return self._files
-
-    @files.setter
-    def files(self, value):
-        if isinstance(value, list):
-            self._files.extend(value)
-        else:
-            self._files.append(value)
-        self._files = list(set(self._files))
-
-    @property
     def source_name_or_path(self):
         return self._source_name_or_path
 
     @source_name_or_path.setter
     def source_name_or_path(self, value):
-        if not isinstance(value, list):
-            value = value.split(",")
-        self._source_name_or_path.extend(value)
-        self._source_name_or_path = [item.strip() for item in self._source_name_or_path]
+        if isinstance(value, list):
+            self._source_name_or_path.extend(value)
+        else:
+            self._source_name_or_path.append(value)
         self._source_name_or_path = list(set(self._source_name_or_path))
 
     def set_sheet_item(self, item):
@@ -129,25 +102,25 @@ class OssItem:
         self.licenses = item[3]
         self.download_location = item[4]
         self.homepage = item[5]
-        self.copyright_text = item[6]
+        self.copyright = item[6]
         self.exclude = item[7]
         self.comment = item[8]
 
     def get_print_array(self):
         items = []
-        if len(self.files) == 0:
-            self.files.append("")
+        if len(self.source_name_or_path) == 0:
+            self.source_name_or_path.append("")
         if len(self.licenses) == 0:
             self.licenses.append("")
 
         exclude = "Exclude" if self.exclude else ""
 
-        for file in self.files:
+        for source_name_or_path in self.source_name_or_path:
             lic = ",".join(self.licenses)
             if self.relative_path != "" and not str(self.relative_path).endswith("/"):
                 self.relative_path += "/"
-            items.append([self.relative_path + file, self.name, self.version, lic, self.source, self.homepage,
-                          self.copyright, "", exclude, self.comment])
+            items.append([self.relative_path + source_name_or_path, self.name, self.version, lic,
+                          self.download_location, self.homepage, self.copyright, "", exclude, self.comment])
         return items
 
     def get_print_json(self):
@@ -155,22 +128,16 @@ class OssItem:
         json_item["name"] = self.name
 
         json_item["version"] = self.version
-        if len(self.files) > 0:
-            json_item["file"] = self.files
         if len(self.source_name_or_path) > 0:
             json_item["source_name_or_path"] = self.source_name_or_path
         if len(self.licenses) > 0:
             json_item["license"] = self.licenses
-        if self.source != "":
-            json_item["source"] = self.source
         if self.download_location != "":
             json_item["download_location"] = self.download_location
         if self.homepage != "":
             json_item["homepage"] = self.homepage
         if self.copyright != "":
-            json_item["copyright"] = self.copyright
-        if self.copyright_text != "":
-            json_item["copyright_text"] = self.copyright_text
+            json_item["copyright_text"] = self.copyright
         if self.exclude:
             json_item["exclude"] = self.exclude
         if self.comment != "":
