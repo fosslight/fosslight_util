8010cc9b7d0addda53ce6bddef5c1dfa64621beb
Jiyeong Seok <jiyeong.seok@lge.com>
2022-06-30 09:54:24 +0900

    Fix the decalring constants name and input of func.
    Signed-off-by: Jiyeong Seok <jiyeong.seok@lge.com>

--- src/fosslight_util/compare_yaml.py
+++ src/fosslight_util/compare_yaml.py
@@ -9,14 +9,14 @@ from fosslight_util.constant import LOGGER_NAME
 from fosslight_util.parsing_yaml import parsing_yml
 
 logger = logging.getLogger(LOGGER_NAME)
-_version = 'version'
-_license = 'license'
-_name = 'name'
+VERSION = 'version'
+LICENSE = 'license'
+NAME = 'name'
 
 
 def compare_yaml(before_file, after_file):
-    before_oss_items, _ = parsing_yml(os.path.basename(before_file), os.path.dirname(before_file))
-    after_oss_items, _ = parsing_yml(os.path.basename(after_file), os.path.dirname(after_file))
+    before_oss_items, _ = parsing_yml(before_file, os.path.dirname(before_file))
+    after_oss_items, _ = parsing_yml(after_file, os.path.dirname(after_file))
 
     before_items = get_merged_item(before_oss_items)
     after_items = get_merged_item(after_oss_items)
@@ -28,47 +28,43 @@ def compare_yaml(before_file, after_file):
         else:
             new_before.append(bi)
 
-    _add = "add"
-    _delete = "delete"
-    _change = "change"
-    _prev = "prev"
-    _now = "now"
+    ADD = "add"
+    DELETE = "delete"
+    CHANGE = "change"
+    PREV = "prev"
+    NOW = "now"
 
-    comp_out = {_add: [], _delete: [], _change: []}
+    comp_out = {ADD: [], DELETE: [], CHANGE: []}
 
-    b_name = list(set([bi[_name] for bi in new_before]))
-    a_name = list(set([ai[_name] for ai in after_items]))
+    b_name = set([bi[NAME] for bi in new_before])
+    a_name = set([ai[NAME] for ai in after_items])
 
-    removed_name = [bn for bn in b_name if bn not in a_name]
-    changed_name = list(set(b_name) ^ set(removed_name))
-    added_name = list(set(a_name) ^ set(changed_name))
+    removed_name = b_name - a_name
+    changed_name = b_name - removed_name
+    added_name = a_name - changed_name
 
     for bi in new_before:
-        if bi[_name] in removed_name:
-            comp_out[_delete].append(bi)
-        elif bi[_name] in changed_name:
-            name_find = False
-            for ci in comp_out[_change]:
-                if ci[_name] == bi[_name]:
-                    bi.pop(_name)
-                    ci[_prev].append(bi)
-                    name_find = True
-                    break
-            if not name_find:
-                item_info = {_name: bi[_name], _prev: [], _now: []}
-                bi.pop(_name)
-                item_info[_prev].append(bi)
-                comp_out[_change].append(item_info)
+        if bi[NAME] in removed_name:
+            comp_out[DELETE].append(bi)
+        elif bi[NAME] in changed_name:
+            filtered = next(filter(lambda oss_dict: oss_dict[NAME] == bi[NAME], comp_out[CHANGE]), None)
+            if filtered:
+                bi.pop(NAME)
+                filtered[PREV].append(bi)
+            else:
+                item_info = {NAME: bi[NAME], PREV: [], NOW: []}
+                bi.pop(NAME)
+                item_info[PREV].append(bi)
+                comp_out[CHANGE].append(item_info)
 
     for ai in after_items:
-        if ai[_name] in added_name:
-            comp_out[_add].append(ai)
-        elif ai[_name] in changed_name:
-            for ci in comp_out[_change]:
-                if ci[_name] == ai[_name]:
-                    ai.pop(_name)
-                    ci[_now].append(ai)
-                    break
+        if ai[NAME] in added_name:
+            comp_out[ADD].append(ai)
+        elif ai[NAME] in changed_name:
+            filtered = next(filter(lambda oss_dict: oss_dict[NAME] == ai[NAME], comp_out[CHANGE]), None)
+            if filtered:
+                ai.pop(NAME)
+                filtered[NOW].append(ai)
 
     return comp_out
 
@@ -76,16 +72,13 @@ def compare_yaml(before_file, after_file):
 def get_merged_item(oss_items):
     item_list = []
     for oi in oss_items:
-        item_info = {_name: oi.name, _version: oi.version, _license: oi.license}
+        item_info = {NAME: oi.name, VERSION: oi.version, LICENSE: oi.license}
 
-        version_find = False
-        for ii in item_list:
-            if oi.name == ii[_name] and oi.version == ii[_version]:
-                ii[_license].extend(oi.license)
-                ii[_license] = list(set(ii[_license]))
-                version_find = True
-                break
-        if not version_find:
+        filtered = next(filter(lambda oss_dict: oss_dict[NAME] == oi.name and oss_dict[VERSION] == oi.version, item_list), None)
+        if filtered:
+            filtered[LICENSE].extend(oi.license)
+            filtered[LICENSE] = list(set(filtered[LICENSE]))
+        else:
             item_list.append(item_info)
 
     return item_list
--- tests/test_compare_yaml.py
+++ tests/test_compare_yaml.py
@@ -3,12 +3,14 @@
 # Copyright (c) 2022 LG Electronics Inc.
 # SPDX-License-Identifier: Apache-2.0
 import os
+import json
 from fosslight_util.compare_yaml import compare_yaml
 
 
 def main():
     cur = os.getcwd()
-    print(compare_yaml(os.path.join(cur, 'before.yaml'), os.path.join(cur, 'after.yaml')))
+    result = compare_yaml(os.path.join(cur, 'before.yaml'), os.path.join(cur, 'after.yaml'))
+    print(json.dumps(result, indent=4, sort_keys=True))
 
 
 if __name__ == '__main__':
