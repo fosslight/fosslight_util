f8b605cad7f64c60ca907907d768b3a1a6d86129
Soim <soim.kim@lge.com>
2024-01-29 17:35:03 +0900

    Check the return value when downloading (#148)

--- tests/test_download.py
+++ tests/test_download.py
@@ -6,10 +6,16 @@ from fosslight_util.download import cli_download_and_extract
 
 
 def main():
-    cli_download_and_extract("https://github.com/LGE-OSS/example",
-                             "test_result/download/example", "test_result/download_log/example")
-    cli_download_and_extract("https://pypi.org/project/filelock/3.4.1",
-                             "test_result/download/filelock", "test_result/download_log/filelock")
+    success, msg, _, _ = cli_download_and_extract("https://github.com/LGE-OSS/example",
+                                                  "test_result/download/example",
+                                                  "test_result/download_log/example")
+    if not success:
+        raise Exception(f"Download failed with git:{msg}")
+    success, msg, _, _ = cli_download_and_extract("https://pypi.org/project/filelock/3.4.1",
+                                                  "test_result/download/filelock",
+                                                  "test_result/download_log/filelock")
+    if not success:
+        raise Exception(f"Download failed with wget:{msg}")
 
 
 if __name__ == '__main__':
