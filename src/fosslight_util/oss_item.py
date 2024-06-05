fb86fb5f1583c7e86e15884b4e58e98edc1fb540
Jaekwon Bang <jaekwon.bang@lge.com>
2024-06-05 17:19:03 +0900

    Change column name

--- src/fosslight_util/oss_item.py
+++ src/fosslight_util/oss_item.py
@@ -217,7 +217,7 @@ class OssItem:
         if len(self.depends_on) > 0:
             json_item["depends on"] = self.depends_on
         if self.purl != "":
-            json_item["purl"] = self.purl
+            json_item["package url"] = self.purl
 
         return json_item
 
