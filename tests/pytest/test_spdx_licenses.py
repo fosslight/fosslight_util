53df5bc1ec4f6760a4bd2a62388036ca068792bd
MoonJeWoong <munjin0201@naver.com>
2024-09-09 19:08:00 +0900

    refactor test_spdx_licenses.py
    Signed-off-by: MoonJeWoong <munjin0201@naver.com>

---
+++ tests/pytest/test_spdx_licenses.py
@@ -0,0 +1,9 @@
+from fosslight_util.spdx_licenses import get_spdx_licenses_json
+
+def test_get_spdx_licenses_json():
+    #when
+    success, error_msg, licenses = get_spdx_licenses_json()
+
+    #then
+    assert success is True
+    assert len(licenses) > 0
