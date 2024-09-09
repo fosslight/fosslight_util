167d97a2c5310d1e687a19e6ddf245988f485822
MoonJeWoong <munjin0201@naver.com>
2024-09-09 20:32:06 +0900

    refactor test_write_yaml.py
    Signed-off-by: MoonJeWoong <munjin0201@naver.com>

---
+++ tests/pytest/test_write_yaml.py
@@ -0,0 +1,19 @@
+import pytest
+from fosslight_util.write_yaml import write_yaml
+
+def test_write_yaml(sheet_list_fixture_for_write_yaml):
+    #given
+    output_file1 = 'test_result/yaml/FL-TEST_yaml.yaml'
+    output_file2 = 'test_result/yaml/FL-TEST2_yaml.yaml'
+    sheet_list1 = sheet_list_fixture_for_write_yaml[0]
+    sheet_list2 = sheet_list_fixture_for_write_yaml[1]
+
+    #when
+    success1, msg1, output1 = write_yaml(output_file1, sheet_list1)
+    success2, msg2, output2 = write_yaml(output_file2, sheet_list2)
+
+    #then
+    assert success1 is True
+    assert success2 is True
+    assert output_file1 in output1
+    assert output_file2 in output2
