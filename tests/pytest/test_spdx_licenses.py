04db26c5352b7be6d11d24b6a48dcbdd87c69bbc
MoonJeWoong <munjin0201@naver.com>
2024-09-21 16:16:58 +0900

    refactor legacy tox test files related for test_spdx_licenses
    Signed-off-by: MoonJeWoong <munjin0201@naver.com>

--- tests/pytest/test_spdx_licenses.py
+++ tests/pytest/test_spdx_licenses.py
@@ -1,5 +1,7 @@
 from fosslight_util.spdx_licenses import get_spdx_licenses_json
 
+# legacy/test_spdx_licenses
+
 def test_get_spdx_licenses_json():
     #when
     success, error_msg, licenses = get_spdx_licenses_json()
