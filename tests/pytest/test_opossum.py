4b268035ebc5039b91e0ddd2ae80c65227892e36
MoonJeWoong <munjin0201@naver.com>
2024-09-21 16:41:29 +0900

    refactor legacy tox test files related for test_opossum
    Signed-off-by: MoonJeWoong <munjin0201@naver.com>

---
+++ tests/pytest/test_opossum.py
@@ -0,0 +1,16 @@
+import os
+from fosslight_util.write_opossum import write_opossum
+
+# legacy/test_opossum
+
+def test_opossum(sheet_list_fixture):
+    #given
+    sheet_contents = sheet_list_fixture[1]
+    output_dir = 'test_result/opossum'
+
+    #when
+    success, msg = write_opossum(output_dir + '/FL-TEST_opossum.json', sheet_contents)
+
+    #then
+    assert success is True
+    assert len(os.listdir(output_dir)) > 0
