e57f92f8ddde16e7821bd1c1a2ae9534a8c73f5c
MoonJeWoong <munjin0201@naver.com>
2024-09-21 17:01:18 +0900

    refactor legacy tox test files related for test_download
    Signed-off-by: MoonJeWoong <munjin0201@naver.com>

--- tests/pytest/test_download.py
+++ tests/pytest/test_download.py
@@ -1,5 +1,9 @@
 import os
+
+import pytest
 from fosslight_util.download import cli_download_and_extract
+
+# legacy/test_download
 
 def test_download_from_github():
     #when
@@ -12,12 +16,18 @@ def test_download_from_github():
     assert success is True
     assert len(os.listdir(target_dir)) > 0
 
-def test_download_from_wget():
+@pytest.mark.parametrize("project_name, project_url",
+                         [("filelock", "https://pypi.org/project/filelock/3.4.1"),
+                          ("dependency", "https://pypi.org/project/fosslight-dependency/3.0.5/"),
+                          ("jackson", "https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind/2.12.2"),
+                          ("pub", "https://pub.dev/packages/file/versions/5.2.1")])
+def test_download_from_wget(project_name, project_url):
+    #given
+    target_dir = "test_result/download/" + project_name
+    log_dir = "test_result/download_log/" + project_name
+
     #when
-    target_dir = "test_result/download/filelock"
-    success, msg, _, _ = cli_download_and_extract("https://pypi.org/project/filelock/3.4.1",
-                                                  target_dir,
-                                                  "test_result/download_log/filelock")
+    success, msg, _, _ = cli_download_and_extract(project_url, target_dir, log_dir)
 
     #then
     assert success is True
