978cc2417f6152f5a397f601fec8d87555a5a422
MoonJeWoong <munjin0201@naver.com>
2024-09-28 00:38:52 +0900

    refactor test_write_output.py, test_output_format.py
    Signed-off-by: MoonJeWoong <munjin0201@naver.com>

--- tests/pytest/conftest.py
+++ tests/pytest/conftest.py
@@ -6,6 +6,8 @@ import shutil
 import pytest
 
 from . import constants
+from fosslight_util.constant import FOSSLIGHT_SOURCE
+from fosslight_util.oss_item import ScannerItem, FileItem, OssItem
 
 set_up_directories = [
     constants.TEST_RESULT_DIR,
@@ -26,3 +28,24 @@ def setup_test_result_dir_and_teardown():
     print("==============tearDown==============")
     for dir in remove_directories:
         shutil.rmtree(dir)
+
+
+@pytest.fixture
+def scan_item():
+    scan_item = ScannerItem(FOSSLIGHT_SOURCE)
+    scan_item.set_cover_pathinfo('tests/test_excel_and_csv', '')
+    scan_item.set_cover_comment('This is a test comment')
+
+    file_item = FileItem('test_result/excel_and_csv')
+
+    oss_item = OssItem("test_name", "1.0.0", "Apache-2.0", "https://abc.com")
+    oss_item.comment = "test_name comment"
+    file_item.oss_items.append(oss_item)
+
+    oss_item2 = OssItem("test_name", "2.0.0", "MIT", "https://abc2.com")
+    file_item.oss_items.append(oss_item2)
+    file_item.comment = "all test comment"
+
+    scan_item.append_file_items([file_item])
+
+    return scan_item
