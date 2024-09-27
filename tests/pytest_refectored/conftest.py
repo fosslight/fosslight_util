d0694e8d337be93bbafdde0b7353063f61c92b55
MoonJeWoong <munjin0201@naver.com>
2024-09-28 01:51:56 +0900

    refactor test_download.py
    Signed-off-by: MoonJeWoong <munjin0201@naver.com>

--- tests/pytest_refectored/conftest.py
+++ tests/pytest_refectored/conftest.py
@@ -1,6 +1,7 @@
 # Copyright (c) 2021 LG Electronics Inc.
 # SPDX-License-Identifier: Apache-2.0
 import os
+import shutil
 
 import pytest
 
@@ -18,15 +19,14 @@ remove_directories = [constants.TEST_RESULT_DIR]
 @pytest.fixture(scope="function", autouse=True)
 def setup_test_result_dir_and_teardown():
     print("==============setup==============")
-
     for dir in set_up_directories:
         os.makedirs(dir, exist_ok=True)
 
     yield
 
     print("==============tearDown==============")
-    # for dir in remove_directories:
-    #     shutil.rmtree(dir)
+    for dir in remove_directories:
+        shutil.rmtree(dir)
 
 
 @pytest.fixture
---
+++ tests/pytest_refectored/test_download.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2021 LG Electronics Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+
+import pytest
+
+from fosslight_util.download import cli_download_and_extract
+from . import constants
+
+
+def test_download_from_github():
+    #when
+    target_dir = os.path.join(constants.TEST_RESULT_DIR, "download/example")
+    success, _, _, _ = cli_download_and_extract("https://github.com/LGE-OSS/example",
+                                                  target_dir,
+                                                  "test_result/download_log/example")
+
+    #then
+    assert success is True
+    assert len(os.listdir(target_dir)) > 0
+
+
+@pytest.mark.parametrize("project_name, project_url",
+                         [("filelock", "https://pypi.org/project/filelock/3.4.1"),
+                          ("dependency", "https://pypi.org/project/fosslight-dependency/3.0.5/"),
+                          ("jackson", "https://mvnrepository.com/artifact/com.fasterxml.jackson.core/jackson-databind/2.12.2"),
+                          ("pub", "https://pub.dev/packages/file/versions/5.2.1")])
+def test_download_from_wget(project_name, project_url):
+    #given
+    target_dir = os.path.join(constants.TEST_RESULT_DIR,
+                              os.path.join("download", project_name))
+    log_dir = os.path.join(constants.TEST_RESULT_DIR,
+                           os.path.join("download_log" + project_name))
+
+    #when
+    success, _, _, _ = cli_download_and_extract(project_url, target_dir, log_dir)
+
+    #then
+    assert success is True
+    assert len(os.listdir(target_dir)) > 0
