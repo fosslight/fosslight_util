cc950e9a4a14f435ad4bedb47ebe227fa400fd5a
MoonJeWoong <munjin0201@naver.com>
2024-09-28 02:31:25 +0900

    refactor test_opossum.py
    Signed-off-by: MoonJeWoong <munjin0201@naver.com>

---
+++ tests/pytest_refectored/test_opossum.py
@@ -0,0 +1,20 @@
+# Copyright (c) 2021 LG Electronics Inc.
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+
+from fosslight_util.write_opossum import write_opossum
+from . import constants
+
+
+def test_opossum(scan_item):
+    # given
+    output_dir = os.path.join(constants.TEST_RESULT_DIR, "opossum")
+    filename_with_dir = os.path.join(output_dir, "FL-TEST_opossum.json")
+
+    # when
+    success, _ = write_opossum(filename_with_dir, scan_item)
+
+    # then
+    assert success is True
+    assert len(os.listdir(output_dir)) > 0
