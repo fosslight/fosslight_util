693352eadd0190af9965e54970465b4b0b7f08ee
MoonJeWoong <munjin0201@naver.com>
2024-09-28 00:45:55 +0900

    refactor test_spdx_licenses.py
    Signed-off-by: MoonJeWoong <munjin0201@naver.com>

---
+++ tests/pytest/test_spdx_licenses.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2021 LG Electronics Inc.
+# SPDX-License-Identifier: Apache-2.0
+from fosslight_util.spdx_licenses import get_spdx_licenses_json
+
+
+def test_get_spdx_licenses_json():
+    #when
+    success, _, licenses = get_spdx_licenses_json()
+
+    #then
+    assert success is True
+    assert len(licenses) > 0
